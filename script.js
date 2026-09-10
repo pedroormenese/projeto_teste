4339c34306498cbd29f3d9f9766fef1cbe45bc31
Pedro <pedro.f.ormenese@gmail.com>
2026-09-10 09:21:50 -0300

    test

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const item = document.querySelector(".item-container");
 let startX;
 let positionX;
 
+item.style.backgroundColor = "red";
 
 item.addEventListener('touchstart', /** @param {TouchEvent} e */(e) => {
     startX = e.touches[0].clientX;
